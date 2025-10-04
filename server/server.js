a04925acae1d273ac6980a7ca1af13cb78503bac
Darkness947 <husdfdf@gmail.com>
2025-10-05 00:21:31 +0300

    DAY 2 Backend Progress

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const connectDB = require('./config/db');
+
+connectDB();
 
 const app = express();
 
@@ -10,7 +14,11 @@ app.get('/', (req, res) => {
     res.json({ message: 'Welcome to the To-Do-List Web App Backend!' });
 });
 
-const PORT = 5000;
+const authRoutes = require('./routes/authRoutes');
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
