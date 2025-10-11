0d3407ffd1ace4db0f57fc1199dc2e2bf0306e2e
Darkness947 <husdfdf@gmail.com>
2025-10-11 03:22:22 +0300

    Update V1.0

--- server/server.js
+++ server/server.js
@@ -7,7 +7,12 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+    origin: ['http://localhost:5173', 'https://to-do-list-dev8.netlify.app'],
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.get('/', (req, res) => {
